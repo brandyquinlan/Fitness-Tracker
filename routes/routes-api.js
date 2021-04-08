5830bb13a559f13fd333c22076e0f697a0a62dc1
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-04-08 14:49:21 -0500

    added aggregate function for total duration

--- routes/routes-api.js
+++ routes/routes-api.js
@@ -3,22 +3,12 @@ const mongoose = require("mongoose");
 const db = require("../models/Workout.js");
 const router = require("express").Router();
 
-  // router.get('/api/workouts', async (req, res) => {
-  //   try {
-  //     const workouts = await db.Workout.find({});
-  //     res.send(workouts);;
-  //   }
-  //   catch (err) {
-  //     res.json(err);
-  //   };
-  // });
-
-  router.put('/api/workouts/:id', async ({ body, params }, res) => {
-    await db.findByIdAndUpdate(
-      params.id,
-      { $push: { exercises: body } },
-      { new: true, runValidators: true }
-    )
+router.put('/api/workouts/:id', async ({ body, params }, res) => {
+  await db.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -30,32 +20,48 @@ const router = require("express").Router();
 
 router.post("/api/workouts", async ({ body }, res) => {
   await db.Workout.create(body)
-      .then(dbWorkout => {
-          res.json(dbWorkout);
-      })
-      .catch(err => {
-          res.status(400).json(err);
-      });
-});
-
-router.get('/api/workouts/range', (req, res) => {
-  db.Workout.find({})
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
     .catch(err => {
-      res.json(err);
+      res.status(400).json(err);
     });
 });
 
+router.get('/api/workouts/range', async (req, res) => {
+  try {
+    await db.Workout.aggregate([{
+      $match: {
+        day: {
+          $gte: new (Date)
+        }
+      }
+    },
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "exercises.duration"
+        }
+      }
+    }
+    ])
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+    }
+      catch (err) {
+        res.json(err);
+      };
+    });
+
 router.delete("/api/workouts", ({ body }, res) => {
   Workout.findByIdAndDelete(body.id)
-  .then(() => {
-    res.json(true);
-  })
-  .catch(err => {
-    res.json(err);
-  });
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
